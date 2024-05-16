5c0e4cd8d713a33a7239ad571970f8d007f97420
Nahel <72753978+Nahelv@users.noreply.github.com>
2024-05-16 12:28:09 +0200

    forgot password + fix code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\ForgotPasswordType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,13 +21,11 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+			return $this->redirectToRoute('app_home');
+        }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
@@ -29,4 +36,49 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+	
+	/**
+	 * @throws UserNotFoundException
+	 * @throws TransportExceptionInterface
+	 */
+	#[Route('/reset-password', name: 'app_forgot_password_request')]
+	public function request(
+		Request $request,
+		UserRepository $userRepository,
+		TokenGeneratorInterface $tokenGenerator,
+		EntityManagerInterface $entityManager,
+		MailerInterface $mailer
+	): Response {
+		$form = $this->createForm(ForgotPasswordType::class)->handleRequest($request);
+		
+		if ($form->isSubmitted() && $form->isValid()) {
+			$email = $form->get('email')->getData();
+			$user = $userRepository->findOneBy(['email' => $email]);
+			
+			if (!$user) {
+				throw new UserNotFoundException('Utilisateur non trouvé.');
+			}
+			
+			$token = $tokenGenerator->generateToken();
+			$user->setResetToken($token);
+			$entityManager->flush();
+			
+			//$url = $this->generateUrl('app_reset_password', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+			
+			$email = (new Email())
+				//->from('mmi22d06@mmi-troyes.fr')
+				->to($user->getEmail())
+				->subject('Réinitialisation de votre mot de passe')
+				->html("Cliquez sur le lien suivant pour réinitialiser votre mot de passe : <a href='$url'>$url</a>");
+			
+			$mailer->send($email);
+			
+			$this->addFlash('success', 'Un email de réinitialisation de votre mot de passe a été envoyé.');
+			return $this->redirectToRoute('app_login');
+		}
+		
+		return $this->render('reset_password/request.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
 }
