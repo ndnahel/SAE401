99e8e6c97c2cd2a6845e971f6d4a4b6d83b582c4
CassandreLAMATY <cassandre.lamaty@gmail.com>
2024-05-16 11:14:13 +0200

    :rocket: wind deg to direction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\Api;
+use App\Service\WindDirection;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    	/**
+    /**
 	 * @var Api
 	 */
 	private Api $api;
+
+	/**
+	 * @var WindDirection
+	 */
+	private WindDirection $windDirection;
 	
 	/**
 	 * @var HttpClientInterface
@@ -26,9 +32,11 @@ class HomeController extends AbstractController
 	 */
 	public function __construct(
 		Api $api,
+		WindDirection $windDirection,
 		HttpClientInterface $client
 	) {
 		$this->api = $api;
+		$this->windDirection = $windDirection;
 		$this->client = $client;
 	}
 
@@ -40,12 +48,15 @@ class HomeController extends AbstractController
 
 		$paris = $this->api->getWeather('Paris', $user ? $user->getUnit() : 'metric', $user ? $user->getLang() : 'fr');
 		$paris = json_decode($paris, true);
+		
+		$paris = $this->windDirection->addCompasPoint($paris);
 
 		$defaultTowns = ['Lyon', 'Marseille', 'Nice', 'Nantes', 'Bordeaux', 'Lille'];
 		$defaultWeathers = [];
 		foreach ($defaultTowns as $town) {
 			$defaultWeathers[$town] = $this->api->getWeather($town, $user ? $user->getUnit() : 'metric', $user ? $user->getLang() : 'fr');
 			$defaultWeathers[$town] = json_decode($defaultWeathers[$town], true);
+			$defaultWeathers[$town] = $this->windDirection->addCompasPoint($defaultWeathers[$town]);
 		}
 		
         return $this->render('home/index.html.twig', [
