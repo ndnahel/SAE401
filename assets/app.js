eeb33419f885b509dfe2ccaf7344b7936e583f0a
Nahel <nelly.delaruelle@gmail.com>
2024-12-19 20:11:49 +0100

    fix code

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,10 @@
 import './styles/global.css';
 import './styles/svg.css';
-import './styles/forecastList.css';
-import './styles/forms.css';
-import './styles/variables.css';
-import './styles/weatherCard.css';
+import './styles/weatherCard.css'
 import './styles/weatherDashboard.css';
+import './styles/forecastList.css';
+
+import './scripts/cityCards';
+import './scripts/favouriteCities';
+import './scripts/forecastListAccordeon';
+import './scripts/swipers';
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
+const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
-    //.enableStimulusBridge('./assets/controllers.json')
     .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
+    .enableSassLoader()
+    .enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction());
-
 
 module.exports = Encore.getWebpackConfig();
