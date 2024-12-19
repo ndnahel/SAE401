c1a7c9faefb5022ab86d08439e9abd81a59ac9a4
Nahel <nelly.delaruelle@gmail.com>
2024-12-19 19:26:44 +0100

    import all css

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,7 @@
 import './styles/global.css';
 import './styles/svg.css';
+import './styles/forecastList.css';
+import './styles/forms.css';
+import './styles/variables.css';
+import './styles/weatherCard.css';
+import './styles/weatherDashboard.css';
